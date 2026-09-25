4735ced8b002167f4cff7f0f17a9881987353471
Chami537 <killerserber@gmail.com>
2026-09-25 17:07:36 +0800

    add sql

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-
+import os
+import psycopg
 app = FastAPI()
 
 @app.get("/")
@@ -32,4 +33,20 @@ def server():
 def xubi():
     return {
         "xubi":"xubimeiyouma"
+    }
+
+
+
+@app.get("/db-health")
+def db_health():
+    database_url = os.getenv("DATABASE_URL")
+
+    with psycopg.connect(database_url) as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT version();")
+            version = cur.fetchone()[0]
+
+    return {
+        "status": "connected",
+        "database": version
     }
